c30f37c8d54f74db4e0d136fede2a56fe5129a1f
Chirag Shah <chiragshah92@gmail.com>
2021-04-04 22:34:43 +0530

    Updates

---
+++ precache-manifest.5c464fae00fb46d8ab186bca0e20fad2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655f271934ae4d9d906225fb58fd8f19",
+    "url": "/chishah92.github.io/index.html"
+  },
+  {
+    "revision": "690121ac064aa0f6e8b1",
+    "url": "/chishah92.github.io/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "41d1263e0b9956125bf9",
+    "url": "/chishah92.github.io/static/js/2.1edb9ffe.chunk.js"
+  },
+  {
+    "revision": "690121ac064aa0f6e8b1",
+    "url": "/chishah92.github.io/static/js/main.728ed631.chunk.js"
+  },
+  {
+    "revision": "4291455f90dbec7255fb",
+    "url": "/chishah92.github.io/static/js/runtime~main.ffbcb0ac.js"
+  }
+]);
